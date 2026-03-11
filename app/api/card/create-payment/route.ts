4e11ed68ea458cc175068b5ab4feae423329e354
ASIRA <asira1031@gmail.com>
2026-03-11 16:53:06 +0800

    fix stripe import

--- app/api/card/create-payment/route.ts
+++ app/api/card/create-payment/route.ts
@@ -1,20 +1,5 @@
-import Stripe from "stripe"
+import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-06-20"
-})
-
-export async function POST(req: Request) {
-
-  const { amount } = await req.json()
-
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: amount * 100,
-    currency: "usd",
-    payment_method_types: ["card"]
-  })
-
-  return Response.json({
-    clientSecret: paymentIntent.client_secret
-  })
-}
+  apiVersion: "2026-02-25.clover",
+});
