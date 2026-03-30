92c5464be244148b35f04a23b90d3d0c35c73cb9
ASIRA <asira1031@gmail.com>
2026-03-30 11:50:16 +0800

    add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,20 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://cryptohost-portal.vercel.app",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://cryptohost-portal.vercel.app/dashboard",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://cryptohost-portal.vercel.app/dashboard/validation",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://cryptohost-portal.vercel.app/dashboard/upload",
+      lastModified: new Date(),
+    },
+  ];
+}
